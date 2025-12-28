ae5834620acd99242666e9c347e273fb932b1a85
ioangheraszim <gijohny17@gmail.com>
2025-12-28 14:15:04 +0200

    feat: implemented javascript for navigation toggling and styled about and header - Implemented JavaScript to toggle navbar between active and inactive states - Styled about and header section of the page in mobile format

---
+++ main.js
@@ -0,0 +1,11 @@
+const navItems = document.querySelector(".nav__items");
+const btnOpen = document.getElementById("btn-open");
+const btnClose = document.getElementById("btn-close");
+
+btnOpen.addEventListener("click", () => {
+  navItems.classList.add("nav__items--active");
+});
+
+btnClose.addEventListener("click", () => {
+  navItems.classList.remove("nav__items--active");
+});
